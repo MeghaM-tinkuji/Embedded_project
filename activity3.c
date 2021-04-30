764714119e25c47325d0cb9f08eecb8577e6365a
Megha M <67791507+MeghaM-tinkuji@users.noreply.github.com>
2021-04-30 22:32:16 +0530

    Add files via upload

---
+++ activity3.c
@@ -0,0 +1,30 @@
+#include "activity3.h"
+
+void pwm_config(void)
+{
+    CONTROL_REG_A|=(1<<COMPARE_OUTPUT_A)|(1<<WAVEFORM_MODE_11)|(1<<WAVEFORM_MODE_10); /* non-inverting mode is set, fast PWM 10 bit */
+    CONTROL_REG_B|=(1<<WAVEFORM_MODE_12)|(1<<CLK_SELECT_11)|(1<<CLK_SELECT_10); /* clk/64 prescalar */
+    WAVEFORM_OUTPUT|=(1<<WAVEFORM_OUTPUT_PIN); /* waverform output */
+}
+
+
+void activity3(volatile uint16_t ADC_value)
+{
+    pwm_config();
+    if(ADC_value<=200)
+    {
+       OUTPUT_COMPARE_REG=0xCD; /* 20% duty cycle */ 
+    }
+    if((ADC_value>=210) && (ADC_value<=500))
+    {
+        OUTPUT_COMPARE_REG=0x19A; /* 40% duty cycle*/
+    }
+    if((ADC_value>=510) && (ADC_value<=700))
+    {
+        OUTPUT_COMPARE_REG=0x2CD; /* 70% duty cycle */
+    }
+    if((ADC_value>=710) && (ADC_value<=1024))
+    {
+        OUTPUT_COMPARE_REG=0x3CD; /* 95% duty cycle */
+    }  
+}
